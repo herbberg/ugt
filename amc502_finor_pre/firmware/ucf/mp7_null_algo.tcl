5cac90a12b9a988ed5c18070c33721d9200d8144
Herbert Bergauer <Herbert.Bergauer@cern.ch>
2018-01-12 11:04:06 +0000

    refs trac-2329 : inserted IOB constraints in mp7_null_algo.tcl http://cms-project-l1osw-trac-backup.web.cern.ch/cms-project-l1osw-trac-backup/tickets/2329.html

--- amc502_finor_pre/firmware/ucf/mp7_null_algo.tcl
+++ amc502_finor_pre/firmware/ucf/mp7_null_algo.tcl
@@ -1,3 +1,16 @@
 # Area constraints for MP7 null algo
 
-add_cells_to_pblock [get_pblock payload] payload
+## HB 2018-01-12: this constraint was moved to area_constraints.tcl (by JW). See directory "replacement_files".
+#add_cells_to_pblock [get_pblock payload] payload
+
+## Input ports finor
+## HB 2016-11-30: only FMC0 used on AMC502 finor preview.
+set_property IOB TRUE [get_ports FMC0_LA00_CC_P]
+set_property IOB TRUE [get_ports FMC0_LA01_CC_P]
+set_property IOB TRUE [get_ports FMC0_LA02_P]
+set_property IOB TRUE [get_ports FMC0_LA03_P]
+set_property IOB TRUE [get_ports FMC0_LA04_P]
+set_property IOB TRUE [get_ports FMC0_LA05_P]
+# Output ports
+set_property IOB TRUE [get_ports FMC0_LA06_P]
+set_property IOB TRUE [get_ports FMC0_LA07_P]

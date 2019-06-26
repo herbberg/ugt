7d9753e40cdd1e48b7a4445a2ae22f76851c38a5
Herbert Bergauer <herbert.bergauer@oeaw.ac.at>
2019-06-26 10:17:00 +0200

    modified for l1menu VHDL files

--- mp7_ugt/firmware/cfg/l1menu_files.tcl
+++ mp7_ugt/firmware/cfg/l1menu_files.tcl
@@ -3,11 +3,7 @@
 set mod_id $env(module_id)
 # puts $mod_id
 
-set gtl_pkg "../../src/module_${mod_id}/gtl_pkg.vhd"
-# puts $gtl_pkg
-set algo_mapping_rop "../../src/module_${mod_id}/algo_mapping_rop.vhd"
-# puts $algo_mapping_rop
-set gtl_module "../../src/module_${mod_id}/gtl_module.vhd"
-# puts $gtl_module
+set l1menu "../../src/module_${mod_id}/l1menu.vhd"
+set l1menu_pkg "../../src/module_${mod_id}/l1menu_pkg.vhd"
 
-add_files -norecurse -fileset sources_1 $gtl_pkg $algo_mapping_rop $gtl_module
+add_files -norecurse -fileset sources_1 $l1menu $l1menu_pkg

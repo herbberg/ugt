f66b2040b0ebe5b68fad0f32fdd1e28e9207d824
Herbert Bergauer <herbert.bergauer@oeaw.ac.at>
2019-08-27 09:00:51 +0200

    inserted cases for "same objects" and "different objects"

--- mp7_ugt/firmware/hdl/data/gtl/diff_phi_lut.vhd
+++ mp7_ugt/firmware/hdl/data/gtl/diff_phi_lut.vhd
@@ -2,6 +2,7 @@
 -- Differences in phi with LUTs.
 
 -- Version-history:
+-- HB 2019-08-27: Cases for "same objects" and "different objects" (less resources for "same objects").
 -- HB 2019-08-22: Updated instance luts_corr_cuts.
 -- HB 2019-08-21: Instantiated luts_corr_cuts.
 -- HB 2019-07-04: Changed type of outputs (compatible with inputs of comparators_corr_cuts.vhd).
@@ -34,9 +35,16 @@ begin
 
     l_1: for i in 0 to N_OBJ_1-1 generate
         l_2: for j in 0 to N_OBJ_2-1 generate
+            same_obj_t: if (OBJ(1) = OBJ(2)) and j>i generate
             lut_i : entity work.luts_corr_cuts
                 generic map(OBJ, deltaPhi)  
                 port map(sub_phi(i,j), diff_phi_i(i,j));
+            end generate same_obj_t;    
+            diff_obj_t: if (OBJ(1) /= OBJ(2)) generate
+            lut_i : entity work.luts_corr_cuts
+                generic map(OBJ, deltaPhi)  
+                port map(sub_phi(i,j), diff_phi_i(i,j));
+            end generate diff_obj_t;    
             l_3: for k in 0 to DETA_DPHI_VECTOR_WIDTH-1 generate
                 diff_phi_o(i,j,k) <= diff_phi_i(i,j)(k);
             end generate l_3;

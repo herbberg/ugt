565f918a8c48a11758426f7915f850dbe9de2ade
Herbert Bergauer <herbert.bergauer@oeaw.ac.at>
2019-08-27 10:25:01 +0200

    inserted cases for "same objects" and "different objects" (2nd part)

--- mp7_ugt/firmware/hdl/data/gtl/invariant_mass.vhd
+++ mp7_ugt/firmware/hdl/data/gtl/invariant_mass.vhd
@@ -2,6 +2,7 @@
 -- Calculation of invariant mass based on LUTs.
 
 -- Version history:
+-- HB 2019-08-27: Cases for "same objects" and "different objects" (less resources for "same objects").
 -- HB 2019-08-20: Changed types.
 -- HB 2019-01-14: No output register.
 -- HB 2018-11-26: First design.
@@ -18,6 +19,7 @@ entity invariant_mass is
     generic(
         N_OBJ_1 : positive;
         N_OBJ_2 : positive;
+        OBJ : obj_type_array;
         PT1_WIDTH : positive;
         PT2_WIDTH : positive;
         COSH_COS_WIDTH : positive
@@ -47,14 +49,26 @@ begin
                 cosh_deta_i(i,j)(k) <= cosh_deta(i,j,k);
                 cos_dphi_i(i,j)(k) <= cos_dphi(i,j,k);
             end generate conv_i;
-            mass_calc_i : entity work.inv_mass_calc
-                generic map(PT1_WIDTH, PT2_WIDTH, COSH_COS_WIDTH, MASS_WIDTH)  
-                port map(
-                    pt1(i)(PT1_WIDTH-1 downto 0), pt2(j)(PT2_WIDTH-1 downto 0),
-                    cosh_deta_i(i,j)(COSH_COS_WIDTH-1 downto 0),
-                    cos_dphi_i(i,j)(COSH_COS_WIDTH-1 downto 0),
-                    invariant_mass_sq_div2(i,j)
-                );
+            same_obj_t: if (OBJ(1) = OBJ(2)) and j>i generate
+                mass_calc_i : entity work.inv_mass_calc
+                    generic map(PT1_WIDTH, PT2_WIDTH, COSH_COS_WIDTH, MASS_WIDTH)  
+                    port map(
+                        pt1(i)(PT1_WIDTH-1 downto 0), pt2(j)(PT2_WIDTH-1 downto 0),
+                        cosh_deta_i(i,j)(COSH_COS_WIDTH-1 downto 0),
+                        cos_dphi_i(i,j)(COSH_COS_WIDTH-1 downto 0),
+                        invariant_mass_sq_div2(i,j)
+                    );
+            end generate same_obj_t;    
+            diff_obj_t: if (OBJ(1) /= OBJ(2)) generate
+                mass_calc_i : entity work.inv_mass_calc
+                    generic map(PT1_WIDTH, PT2_WIDTH, COSH_COS_WIDTH, MASS_WIDTH)  
+                    port map(
+                        pt1(i)(PT1_WIDTH-1 downto 0), pt2(j)(PT2_WIDTH-1 downto 0),
+                        cosh_deta_i(i,j)(COSH_COS_WIDTH-1 downto 0),
+                        cos_dphi_i(i,j)(COSH_COS_WIDTH-1 downto 0),
+                        invariant_mass_sq_div2(i,j)
+                    );
+            end generate diff_obj_t;    
             l_3: for k in 0 to MASS_WIDTH-1 generate
                 inv_mass_o(i,j,k) <= invariant_mass_sq_div2(i,j)(k);                 
             end generate l_3;
--- mp7_ugt/firmware/hdl/data/gtl/sub_phi.vhd
+++ mp7_ugt/firmware/hdl/data/gtl/sub_phi.vhd
@@ -2,7 +2,7 @@
 -- Differences in phi.
 
 -- Version-history:
--- HB 2019-08-26: Instantiated sub_phi_calc.
+-- HB 2019-08-27: Cases for "same objects" and "different objects" (less resources for "same objects").
 -- HB 2019-06-27: Changed type of outputs.
 -- HB 2019-01-11: First design.
 
@@ -10,13 +10,13 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 use work.gtl_pkg.all;
-use work.lut_pkg.all;
 
 entity sub_phi is
     generic(
         N_OBJ_1 : positive;
         N_OBJ_2 : positive;
-        PHI_HALF_RANGE : positive := 72
+        OBJ : obj_type_array;
+        PHI_HALF_RANGE : positive
     );
     port(
         phi_1 : in conv_integer_array;
@@ -27,17 +27,20 @@ end sub_phi;
 
 architecture rtl of sub_phi is
 
-    type sub_phi_array is array (0 to N_OBJ_1-1, 0 to N_OBJ_2-1) of integer;
-    signal sub_phi : sub_phi_array;
-
+    signal sub_temp : max_phi_range_array := (others => (others => 0));
+    
 begin
     
     loop_1: for i in 0 to N_OBJ_1-1 generate
         loop_2: for j in 0 to N_OBJ_2-1 generate
-            sub_phi_calc_i : entity work.sub_phi_calc
-                generic map(PHI_HALF_RANGE)
-                port map(phi_1(i), phi_2(j), sub_phi(i,j));
-            sub_phi_o(i,j) <= sub_phi(i,j);
+            same_obj_t: if (OBJ(1) = OBJ(2)) and j>i generate
+                sub_temp(i,j) <= abs(phi_1(i) - phi_2(j));
+                sub_phi_o(i,j) <= sub_temp(i,j) when (sub_temp(i,j) < PHI_HALF_RANGE) else (PHI_HALF_RANGE*2-sub_temp(i,j));
+            end generate same_obj_t;    
+            diff_obj_t: if (OBJ(1) /= OBJ(2)) generate
+                sub_temp(i,j) <= abs(phi_1(i) - phi_2(j));
+                sub_phi_o(i,j) <= sub_temp(i,j) when (sub_temp(i,j) < PHI_HALF_RANGE) else (PHI_HALF_RANGE*2-sub_temp(i,j));
+            end generate diff_obj_t;    
         end generate loop_2;
     end generate loop_1;
 

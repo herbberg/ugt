043f271e94fc40ad5b9d886955bff3fbb5069cf4
Herbert Bergauer <herbert.bergauer@oeaw.ac.at>
2019-09-25 14:24:54 +0200

    updated

--- mp7_ugt/firmware/hdl/data/gtl/inv_mass_calc.vhd
+++ mp7_ugt/firmware/hdl/data/gtl/inv_mass_calc.vhd
@@ -30,22 +30,22 @@ end inv_mass_calc;
 
 architecture rtl of inv_mass_calc is
 
---     signal cosh_cos : std_logic_vector(COSH_COS_WIDTH-1 downto 0);
+    signal cosh_cos : std_logic_vector(COSH_COS_WIDTH-1 downto 0);
 
 -- HB 2017-09-21: used attribute "use_dsp" instead of "use_dsp48" for "mass" - see warning below
 -- MP7 builds, synth_1, runme.log => WARNING: [Synth 8-5974] attribute "use_dsp48" has been deprecated, please use "use_dsp" instead
     attribute use_dsp : string;
---     attribute use_dsp of cosh_cos : signal is "yes";
+    attribute use_dsp of cosh_cos : signal is "yes";
     attribute use_dsp of inv_mass_sq_div2 : signal is "yes";
 
 begin
 
 -- HB 2015-10-01: calculation of invariant mass with formular M**2/2=pt1*pt2*(cosh(eta1-eta2)-cos(phi1-phi2))
 --     inv_mass_sq_div2 <= pt1(PT1_WIDTH-1 downto 0) * pt2(PT2_WIDTH-1 downto 0) * ((cosh_deta(COSH_COS_WIDTH-1 downto 0)) - (cos_dphi(COSH_COS_WIDTH-1 downto 0)));
---     cosh_cos <= cosh_deta - cos_dphi;
---     inv_mass_sq_div2 <= pt1 * pt2 * cosh_cos;
+    cosh_cos <= cosh_deta - cos_dphi;
+    inv_mass_sq_div2 <= pt1 * pt2 * cosh_cos;
            
--- HB 2015-10-01: calculation of invariant mass with formular M**2/2=pt1*pt2*(cosh(eta1-eta2)-cos(phi1-phi2))
-    inv_mass_sq_div2 <= pt1 * pt2 * (cosh_deta - cos_dphi);
+-- -- HB 2015-10-01: calculation of invariant mass with formular M**2/2=pt1*pt2*(cosh(eta1-eta2)-cos(phi1-phi2))
+--     inv_mass_sq_div2 <= pt1 * pt2 * (cosh_deta - cos_dphi);
 
 end architecture rtl;

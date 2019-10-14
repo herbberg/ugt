c0b444a6d1fcb30ebad3c69c02ea32dd11cc4529
Herbert Bergauer <herbert.bergauer@oeaw.ac.at>
2019-10-14 11:55:05 +0200

    created branch and modified twobody_pt_calc.vhd for optimizing resources

--- firmware/hdl/data/gtl/twobody_pt_calc.vhd
+++ firmware/hdl/data/gtl/twobody_pt_calc.vhd
@@ -32,6 +32,7 @@ end twobody_pt_calc;
 
 architecture rtl of twobody_pt_calc is
 
+    signal cos_phi_1x2, sin_phi_1x2 : integer;
     signal cos_plus_sin_vec_temp : std_logic_vector(2*SIN_COS_WIDTH-1 downto 0);
     signal cos_plus_sin_vec : std_logic_vector(2*SIN_COS_WIDTH-1 downto 0);
     signal pt1_sq : std_logic_vector(PT1_WIDTH+PT1_WIDTH-1 downto 0);
@@ -48,12 +49,11 @@ architecture rtl of twobody_pt_calc is
 -- HB 2017-09-21: used attribute "use_dsp" instead of "use_dsp48" for "mass" - see warning below
 -- MP7 builds, synth_1, runme.log => WARNING: [Synth 8-5974] attribute "use_dsp48" has been deprecated, please use "use_dsp" instead attribute
     attribute use_dsp : string;
---     attribute use_dsp of cos_plus_sin_vec : signal is "yes";
+--     attribute use_dsp of cos_phi_1x2 : signal is "yes";
+--     attribute use_dsp of sin_phi_1x2 : signal is "yes";
     attribute use_dsp of pt1_pt2_cos_sin : signal is "yes";
-    attribute use_dsp of pt1_sq : signal is "yes";
-    attribute use_dsp of pt2_sq : signal is "yes";
---     attribute use_dsp of pt_sq : signal is "yes";
---     attribute use_dsp of twobody_pt_sq : signal is "yes";
+--     attribute use_dsp of pt1_sq : signal is "yes";
+--     attribute use_dsp of pt2_sq : signal is "yes";
 
 begin
 
@@ -62,7 +62,9 @@ begin
 --               conversion cos_plus_sin_integer to cos_plus_sin_vec, depending on pos. or neg. value of cos_plus_sin_integer
 --               twobody_pt_sq = pt1**2+pt2**2+2*pt1*pt2*cos_plus_sin_vec
 
-    cos_plus_sin_vec_temp <= CONV_STD_LOGIC_VECTOR(((cos_phi_1*cos_phi_2) + (sin_phi_1*sin_phi_2)), (2*sin_cos_width));
+    cos_phi_1x2 <= cos_phi_1*cos_phi_2;
+    sin_phi_1x2 <= sin_phi_1*sin_phi_2;
+    cos_plus_sin_vec_temp <= CONV_STD_LOGIC_VECTOR((cos_phi_1x2 + sin_phi_1x2), 2*sin_cos_width);
 -- HB 2017-03-22: use two's complement when cos_plus_sin_vec_temp is negative
     cos_plus_sin_vec <= cos_plus_sin_vec_temp when cos_plus_sin_vec_temp(cos_plus_sin_vec_temp'high) = '0' else (not(cos_plus_sin_vec_temp)+1);
     pt1_pt2_cos_sin_temp <= conv_std_logic_vector(2,2) * pt1(PT1_WIDTH-1 downto 0) * pt2(PT2_WIDTH-1 downto 0) * cos_plus_sin_vec;
